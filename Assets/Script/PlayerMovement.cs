2c2607c91c5981d182f4a2cb34320fbdbe84c617
Kristian Numme <95479930+Kristiangit@users.noreply.github.com>
2023-09-14 13:36:13 +0200

    cornerposition

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -8,37 +8,41 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 pos;
     private Rigidbody rb;
 
+    public float obsPos;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
+        transform.position = new Vector3(obsPos, obsPos -1f, 0);
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        float obsX = obsPos;
+        float obsY = obsPos -1f;
 
         pos = transform.position;
 
         if (Input.GetKey(KeyCode.UpArrow)) // Up
         {
-            pos.y = 1.5f;  
+            pos.y = obsY + 0.5f;  
         }
 
         if (Input.GetKey(KeyCode.DownArrow)) // Down
         {
-            pos.y = 0.5f;
+            pos.y = obsY - 0.5f;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow)) // Left
         {
-            pos.x = 9.5f;
+            pos.x = obsX - 0.5f;
         }
 
         if (Input.GetKey(KeyCode.RightArrow)) // Right
         {
-            pos.x = 10.5f;
+            pos.x = obsX + 0.5f;
         }
 
 
